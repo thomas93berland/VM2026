39f8e00af69ad9d114ecdce33c34c3afd127ab52
thomas93berland <thomas93berland@gmail.com>
2026-06-14 20:20:02 +0200

    Load recent bets activity feed

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -56,4 +56,5 @@ window.VM_LIVE_SCORE_FUNCTION_URL = "https://us-central1-the-club-17c87.cloudfun
   load('homeLiveScoreFixScript', 'js/home-live-score-fix.js?v=1');
   load('liveScoreApiScript', 'js/live-score-api.js?v=1');
   load('homeLiveCleanNamesScript', 'js/home-live-clean-names.js?v=1');
+  load('recentBetsActivityScript', 'js/recent-bets-activity.js?v=1');
 })();
